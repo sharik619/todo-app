ca9cbd4e309722b1ee19b8ecb2c6561df2063911
Sharik Ansari <sharikansari619@gmail.com>
2023-12-27 18:06:18 +0530

    user input and output show

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # Take a input from user and store it into 'title'
-
-
+title = input('title:')
 # Take another input from user and store it into 'description'
-
-
+description = input('Description:')
 # Once done, print Todo Title and Discription
-
+print(title,":", description)
