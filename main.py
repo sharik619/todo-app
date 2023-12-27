f376685f7f078be26d484fe49e259df2d349c533
Kamal Kumar <kamal2k89@gmail.com>
2023-12-27 20:31:47 +0530

    Task 2 completed

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ option = int(input('Todo App\n  1. Create\n  2. List\n: '))
 
 
 if option == 1:
-    print('Create')
+    title = input('Enter title: ')
+    description = input('Enter description: ')
+    with open('todos.txt', 'a') as f:
+        f.write(title)
+        f.write('|')
+        f.write(description)
+        f.write('\n')
+    print('Task saved.')
 elif option == 2:
-    print('List')
+    with open('todos.txt', 'r') as f:
+        print(f.read())
 else:
     print('Please select 1 or 2 only.')
