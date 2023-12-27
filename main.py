2a5763f55e7bb4cf33437eef3d0a36a80e30e46b
Kamal Kumar <kamal2k89@gmail.com>
2023-12-27 18:19:25 +0530

    Task 2

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Take a input from user and store it into 'title'
-title = input('title:')
+title = input('Title: ')
+
 # Take another input from user and store it into 'description'
-description = input('Description:')
+description = input('Description: ')
+
 # Once done, print Todo Title and Discription
-print(title,":", description)
+print(title, ":", description)
+
+# Write it into a file so that tasks get preserved.
